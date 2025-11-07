5b66112e49b85784ebe73bd88cb7b31a5210bddb
Andrija <andrijadinic30@gmail.com>
2025-11-07 11:08:29 +0100

    improvements

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ require('config.lazy')
 
 vim.opt.shiftwidth=2
 vim.opt.smartindent=false
+
+-- Avoid Copilot clashes, also honestly it's sometimes annoying when you want to press tab
+-- and it applies the suggestions without you wanting to
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,3 @@
 return {
-  "github/copilot.vim",
-  config = function()
-    vim.g.copilot_no_tab_map = true
-    vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
-  end
+  "github/copilot.vim"
 }
