29b4607b50275bf3bc8719fcd58bf66eeb0943af
Andrija <andrijadinic30@gmail.com>
2025-11-27 12:39:56 +0100

    Updates

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -8,6 +8,28 @@ return {
 	  width = 0.25
 	}
       }
+    },
+    -- This doesn't work, for some reason
+    memory = {
+      opts = {
+	chat = {
+	  default_memory = {'default'}
+	}
+      }
+    },
+    -- This whole ordeal is for setting the default Copilot model
+    adapters = {
+      http = {
+	copilot = function()
+	  return require('codecompanion.adapters').extend('copilot', {
+	    schema = {
+	      model = {
+		default = 'gpt-5-mini'
+	      }
+	    }
+	  })
+	end
+      }
     }
   },
   cmd = {'CodeCompanion', 'CodeCompanionChat'},
