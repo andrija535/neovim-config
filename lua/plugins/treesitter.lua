fa3f2ed7f48e6002fc0ce5c0f26df7bcd9f65615
Andrija <andrijadinic30@gmail.com>
2025-12-01 09:30:53 +0100

    Fix TS

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,16 +2,16 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   branch = 'main',
+  version = false,
   build = ':TSUpdate',
-  opts = {
-    highlight = {
-      enable = true
-    },
-    indent = {
-      enable = true
-    },
-    ensure_installed = {
-      'php'
-    }
-  },
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {'php'},
+      callback = function() 
+	vim.treesitter.start()
+	vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+	vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
+    })
+  end
 }
