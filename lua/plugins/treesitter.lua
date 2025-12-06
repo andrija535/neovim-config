6aa6e722aa484d651a3f219adcc5a674977d01d2
Andrija <andrijadinic30@gmail.com>
2025-12-07 00:47:58 +0100

    Update treesitter for OCaml

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,11 @@ return {
   version = false,
   build = ':TSUpdate',
   config = function()
+    require('nvim-treesitter').install {'php', 'ocaml'}
+
     vim.api.nvim_create_autocmd('FileType', {
-      pattern = {'php'},
-      callback = function() 
+      pattern = {'php', 'ocaml'},
+      callback = function()
 	vim.treesitter.start()
 	vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 	vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
